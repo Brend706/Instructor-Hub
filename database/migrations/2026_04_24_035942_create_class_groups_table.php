34e23d773c06e63699a6f1bac6849c535c8e97c5
lv19015 <lv19015@ues.edu.sv>
2026-04-23 22:41:03 -0600

    Creacion de migraciones iniciales y configuracion de .env

---
+++ database/migrations/2026_04_24_035942_create_class_groups_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('class_groups', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('career');
+            $table->string('semester');
+            $table->string('modality');
+            $table->string('schedule');
+            $table->string('classroom');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('class_groups');
+    }
+};
---
+++ database/migrations/2026_04_24_040049_create_sessions_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sessions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('instructor_assignment_id')->constrained('instructor_assignments')->onDelete('cascade');
+            $table->date('date');
+            $table->time('start_time');
+            $table->time('end_time');
+            $table->string('classroom')->nullable(); //si es presencial
+            $table->string('virtual_link')->nullable(); //si es virtual
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sessions');
+    }
+};
